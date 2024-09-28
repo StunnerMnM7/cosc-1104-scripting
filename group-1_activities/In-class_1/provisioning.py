9ca67705c25279b89b0763664a4652b60e460233
Mihir Mane <mihirmane3@gmail.com>
2024-09-27 22:56:02 -0500

    *

--- group-1_activities/In-class_1/provisioning.py
+++ group-1_activities/In-class_1/provisioning.py
@@ -5,8 +5,8 @@ Name: Mihir Mane(100947380), Zuhaib Khan(100935821)
 # Description: This script is a provisioning system, allowing users to request CPU cores and memory.
 '''
 #Total Resources
-total_cpu = 8
-total_memory = 32
+total_cpu = 32
+total_memory = 64
 required_cpu = int(input("Enter number of CPUs required: "))
 required_mem = float(input("Enter amount of memory requred in GB: "))
 available_cpu = 0
@@ -14,11 +14,13 @@ available_mem = 0
 
 while True:
     if required_cpu <= 0 or required_mem <= 0:
-        print("Invalid resource request. Resource request cannot be negative.")
+        print("Invalid resource request. Resource request cannot be negative.")y
+        break
         
     elif required_cpu > total_cpu or required_mem > total_memory:
         print("Resources exceeds availability limit.\n")
         print(f"Available Resources: CPU cores - {total_cpu} | Memory - {total_memory}\n")
+        break
             
     elif required_cpu <= total_cpu and required_mem <= total_memory: 
         available_cpu = total_cpu - required_cpu
@@ -27,38 +29,3 @@ while True:
         print(f"Available Resources: CPU cores - {available_cpu} | Memory - {available_mem}\n")   
         break
     
-    
-    
-
-'''
-# provisioning.py
-# Author: Mihir Mane
-# Date: [Insert Date]
-
-# Constants
-TOTAL_CPU_CORES = 8  # Total number of CPU cores available
-TOTAL_MEMORY_GB = 32.0  # Total amount of memory available in gigabytes (GB)
-
-# User input
-required_cpu_cores = int(input("Enter the number of required CPU cores: "))
-required_memory_gb = float(input("Enter the amount of required memory in GB: "))
-
-# Input validation
-if required_cpu_cores < 0 or required_memory_gb < 0:
-    print("Error: Resource requests cannot be negative.")
-else:
-    # Resource availability check
-    if (required_cpu_cores <= TOTAL_CPU_CORES) and (required_memory_gb <= TOTAL_MEMORY_GB):
-        print("Resources provisioned successfully.")
-    else:
-        print("Resource request exceeds capacity. Provisioning failed.")
-
-    # Calculate remaining resources
-    remaining_cpu_cores = TOTAL_CPU_CORES - required_cpu_cores if required_cpu_cores <= TOTAL_CPU_CORES else TOTAL_CPU_CORES
-    remaining_memory_gb = TOTAL_MEMORY_GB - required_memory_gb if required_memory_gb <= TOTAL_MEMORY_GB else TOTAL_MEMORY_GB
-
-    # Display remaining resources
-    print(f"Remaining CPU cores: {remaining_cpu_cores}")
-    print(f"Remaining memory: {remaining_memory_gb} GB")
-
-'''
